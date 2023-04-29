b451874760749fd195df2a9aae094217f14fb9be
OwenTruong <44269025+OwenTruong@users.noreply.github.com>
2023-04-29 17:51:00 -0400

    my template

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-blue-500">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/webpack.config.js
@@ -0,0 +1,40 @@
+const webpack = require('webpack');
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+const mode = 'development';
+
+const config = {
+  mode: mode,
+  target: 'node',
+  entry: './src/app.ts',
+  externals: [nodeExternals()],
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+    extensions: ['.ts', '.js'],
+  },
+
+  devtool: 'source-map',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs',
+  },
+};
+
+module.exports = config;
